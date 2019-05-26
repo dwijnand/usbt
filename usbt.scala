e6ca991852ea0b1ed1ac76ed346b2b34edc2578f
Dale Wijnand <dale.wijnand@gmail.com>
2019-05-26 20:41:12 +0100

    Remove and undelete

--- usbt.scala
+++ usbt.scala
@@ -22,6 +22,22 @@ final case class Key[A](name: String, scope: Scope = This) extends Init[A] {
 final case class Setting[A](key: Key[A], init: Init[A])
 
 object Main {
+
+  def anyToString(x: Any) = x match {
+    case s: String => s""""$s""""
+    case _         => s"$x"
+  }
+
+  def keyToString(key: Key[_]) = s"""${key.scope} / ${key.name}"""
+
+  def initToString(init: Init[_], addOp: Boolean = false): String = init match {
+    case Init.Value(x)        => (if (addOp) "  := " else "") + anyToString(x)
+    case Init.Mapped(init, _) => (if (addOp) " <<= " else "") + initToString(init) + ".map(<f>)"
+    case key: Key[_]          => (if (addOp) " <<= " else "") + keyToString(key)
+  }
+
+  def settingToString(s: Setting[_]) = keyToString(s.key) + initToString(s.init, addOp = true)
+
   def main(args: Array[String]): Unit = {
     val baseDir = Key[String]("baseDir")
     val  srcDir = Key[String]( "srcDir")
@@ -33,26 +49,20 @@ object Main {
 
     println(settings.map(settingToString).mkString("[\n  ", "\n  ", "\n]"))
 
+    import scala.collection.mutable.Builder
+    val zero = Map.empty[Key[_], Builder[Setting[_], Seq[Setting[_]]]]
+    val map: Map[Key[_], Seq[Setting[_]]] = settings
+        .foldLeft(zero)((acc, s) => acc.updated(s.key, acc.getOrElse(s.key, Seq.newBuilder) += s))
+        .iterator
+        .map { case (key, builder) => key -> builder.result() }
+        .toMap
+    println(map.map(kv => keyToString(kv._1) -> kv._2.map(settingToString).mkString("[\n    ", "\n    ", "\n  ]")).mkString("Map(\n  ", "\n  ", "\n)"))
+
     def check[A](key: Key[A], expected: A) = {
       val actual = null.asInstanceOf[A]
       if (actual != expected) println(s"Expected $expected, Actual $actual")
     }
 
     check(srcDir in ThisBuild, "/src")
-  }
-
-  def settingToString(s: Setting[_]) = keyToString(s.key) + initToString(s.init, addOp = true)
-
-  def keyToString(key: Key[_]) = s"""${key.scope} / ${key.name}"""
-
-  def initToString(init: Init[_], addOp: Boolean = false): String = init match {
-    case Init.Value(x)        => (if (addOp) "  := " else "") + anyToString(x)
-    case Init.Mapped(init, _) => (if (addOp) " <<= " else "") + initToString(init) + ".map(<f>)"
-    case key: Key[_]          => (if (addOp) " <<= " else "") + keyToString(key)
-  }
-
-  def anyToString(x: Any) = x match {
-    case s: String => s""""$s""""
-    case _         => s"$x"
   }
 }
